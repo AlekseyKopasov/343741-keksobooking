bedc18a42b82aeca49e12c8574b2f9868c07e91c
Aleksey Kopasov <alesey.k1@gmail.com>
2019-01-06 06:16:59 +0300

    chore: вынес всю основную логику в main.js

--- js/map.js
+++ js/map.js
@@ -3,72 +3,13 @@
 (function () {
   var mapElement = document.querySelector('.map');
 
-  var deactivatePage = function () {
-    window.popup.close();
-    window.pins.remove();
-    window.mainPin.resetPosition();
-    window.form.setAddressValue(window.mainPin.getDefaultPosition());
-    window.form.deactivate();
-    window.formPhoto.deactivate();
-    isMapActive = false;
-    mapElement.classList.add('map--faded');
-    window.filter.deactivate();
-  };
-
-  var onFilter = function (filteredOffers) {
-    window.popup.close();
-    window.pins.remove();
-    window.pins.create(filteredOffers);
-  };
-
-  var onGetOffersSuccess = function (offers) {
-    window.filter.activate(offers, onFilter);
-    window.pins.create(offers);
-  };
-
-  var onGetOffersError = function () {
-    window.message.createErrorMessage();
-  };
-
-  var onPostOfferSuccess = function () {
-    mapElement.classList.add('map--faded');
-    isMapActive = false;
-    window.messages.createSuccessMessage();
-
-    window.pins.remove();
-    window.mainPin.resetPosition();
-    window.form.deactivate(window.mainPin.getDefaultPosition());
-    window.filter.deactivate();
-
-  };
-
-  var onPostOfferError = function () {
-    window.messages.createErrorMessage();
-  };
-
-  var callbackFormSubmit = function (data) {
-    window.backend.postOffer(data, onPostOfferSuccess, onPostOfferError);
-    deactivatePage();
-  };
-
-  var callbackFormReset = function () {
-    deactivatePage();
-  };
-
-  var isMapActive = false;
-
-  var callbackMainPinMouseUp = function (mainPinPosition) {
-    if (!isMapActive) {
+  window.map = {
+    activate: function () {
       mapElement.classList.remove('map--faded');
-      window.backend.getOffers(onGetOffersSuccess, onGetOffersError);
-      window.form.activate(callbackFormSubmit, callbackFormReset);
-      window.formPhoto.activate();
+    },
+    deactivate: function () {
+      mapElement.classList.add('map--faded');
     }
-
-    isMapActive = true;
-    window.form.setAddressValue(mainPinPosition);
   };
 
-  window.mainPin.activate(callbackMainPinMouseUp);
-  deactivatePage();
 })();
